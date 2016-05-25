a27de658525cdd9d82d8068f0f817ff46d0ba55d
Linnea Svensson <svensson.47@hotmail.com>
2016-05-25 09:28:13 +0200

    created the ball list and some other stuff

--- src/DummyModel.java
+++ src/DummyModel.java
@@ -7,19 +7,18 @@ public class DummyModel implements IBouncingBallsModel {
 	private final double areaWidth;
 	private final double areaHeight;
 	private final double gravity;
+	private List<BouncingBall> ballList;
 
-	private double x, y, vx, vy, r;
 
 	public DummyModel(double width, double height) {
 		this.areaWidth = width;
 		this.areaHeight = height;
-		gravity = 9.82;
-		x = 1;
-		y = 1;
-		vx = 2.3;
-		vy = 1;
-		r = 1;
+		gravity = -9.82;
+		//Skapar listan med bollar och addar dem
+		ballList = new LinkedList<BouncingBall>();
+		ballList.add(new BouncingBall(1.0,1.0,2.3,1.0,1.0));
 	}
+	
 
 	@Override
 	public void tick(double deltaT) {
@@ -32,11 +31,20 @@ public class DummyModel implements IBouncingBallsModel {
 		x += vx * deltaT;
 		y += vy * deltaT;
 	}
-
+	
+	//För alla bollar i listan, lägg dem i myballs med rätt x, y, r.
 	@Override
 	public List<Ellipse2D> getBalls() {
 		List<Ellipse2D> myBalls = new LinkedList<Ellipse2D>();
-		myBalls.add(new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r));
+		double r;
+		double x;
+		double y;
+		for(BouncingBall ball : ballList){
+			r = ball.getRadius();
+			x = ball.getX();
+			y = ball.getY();
+			myBalls.add(new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r));
+		}
 		return myBalls;
 	}
 }
