3b3672ddd2739e381ca7f00c1abd8da872833623
Linnea Svensson <svensson.47@hotmail.com>
2016-05-27 13:02:01 +0200

    working!!

--- src/DummyModel.java
+++ src/DummyModel.java
@@ -7,42 +7,62 @@ public class DummyModel implements IBouncingBallsModel {
 
 	private final double areaWidth;
 	private final double areaHeight;
-	private final double gravity;
-	private List<BouncingBall> ballList;
-	private double r,angle;
-	private boolean isCollided;
+	private final double gravity = 1;
+	private BouncingBall[] ballList;
+	private double dist,angle;
+
 
 
 	public DummyModel(double width, double height) {
 		this.areaWidth = width;
 		this.areaHeight = height;
-		gravity = -0.82;
 		//Skapar listan med bollar och addar dem
-		ballList = new LinkedList<BouncingBall>();
-		ballList.add(new BouncingBall(3.0,10.0,4.3,10.0,1.0,1.0));
-		ballList.add(new BouncingBall(5.0,10.0,7.3,10.0,1.0,2.0));
+		ballList = new BouncingBall[2];
+		ballList[0] = new BouncingBall(5.0,6.0,5.0,14.0,1.0,20.0);
+		ballList[1]=new BouncingBall(10.0,-8.0,10.0,12.0,1.5,30.0);
 	}
+	@Override
+	public void tick(double deltaT) {
+		boolean isCollided = false;
+		//System.out.println("Start tick");
 
+		for (BouncingBall ball : ballList) {
+			ball.setX(ball.getX() + (ball.getVx() * deltaT));
+			ball.setY(ball.getY() + (ball.getVy() * deltaT));
+		}
 
-	public void rectToPolar(BouncingBall ball) {
-		r= Math.sqrt(ball.getVx()*ball.getVx()+ball.getVy()*ball.getVy());
-		angle= Math.atan(ball.getY()/ball.getX());
-		if (ball.getVx()<0){
-			angle+=Math.PI;
+		for (int i = 0; i<ballList.length-1; i++) {
+			BouncingBall ball1 = ballList[i];
+			for (int j = i+1; j<ballList.length; j++) {
+				BouncingBall ball2 = ballList[j];
+				System.out.println(i + " " + j);
+				if (ball1.isColliding(ball2)) {
+					collision(ball1, ball2);
+					isCollided = true;
+				}
+			}
+
+		}
+
+		for (BouncingBall ball : ballList) {
+			if (ball.getX() < ball.getRadius() || ball.getX() > areaWidth - ball.getRadius()) {
+				System.out.println("in x wall");
+				ball.setVx(ball.getVx() * -1);
+				isCollided = true;
+				
+			}
+			if (ball.getY() < ball.getRadius() || ball.getY() > areaHeight - ball.getRadius()) {
+				System.out.println("in y wall");
+				ball.setVy(ball.getVy() * -1);
+				isCollided = true;
+			}
+		}
+		if (!isCollided) {
+			for (BouncingBall ball : ballList) {
+				ball.setVy(ball.getVy() - (ball.getMass() * deltaT * gravity));
+			}
 		}
 	}
-
-	public void polarToRect(BouncingBall ball) {
-		ball.setVx(r*Math.cos(angle));
-		ball.setVy(r*Math.sin(angle));
-	}
-
-	public void rotate(double rotateAngle, BouncingBall ball) {
-		rectToPolar(ball);
-		angle+=rotateAngle;
-		polarToRect(ball);
-	}
-
 	public void collision(BouncingBall ball1, BouncingBall ball2) {
 		System.out.println("KROCK");
 		double deltaX, deltaY, rotAngle, I, R;
@@ -54,7 +74,7 @@ public class DummyModel implements IBouncingBallsModel {
 		rotate(-rotAngle, ball1);
 		rotate(-rotAngle, ball2);
 
-		I = ball1.getMass() * ball1.getVx() + ball2.getMass() * ball2.getVx();
+		I = ball1.getMass() * ball1.getVx() + (ball2.getMass() * ball2.getVx());
 		R = -(ball2.getVx() - ball1.getVx());
 
 		ball1.setVx((I - R * ball2.getMass()) / (ball1.getMass() + ball2.getMass()));
@@ -63,63 +83,26 @@ public class DummyModel implements IBouncingBallsModel {
 		rotate(rotAngle, ball1);
 		rotate(rotAngle, ball2);
 	}
-
-	public boolean isColliding(BouncingBall ball1, BouncingBall ball2) {
-		if (ball1.getX() + ball1.getRadius() + ball2.getRadius() > ball2.getX()
-				&& ball1.getX() < ball2.getX() + ball1.getRadius() + ball2.getRadius()
-				&& ball1.getY() + ball1.getRadius() + ball2.getRadius() > ball2.getY()
-				&& ball1.getY() < ball2.getY() + ball1.getRadius() + ball2.getRadius())
-		{
-			double cathetus1 = Math.pow(ball1.getX() - ball2.getX(),2);
-			double cathetus2 = Math.pow(ball1.getY() - ball2.getY(),2);
-			double hypotenuse = Math.sqrt(cathetus1 + cathetus2);
-			return hypotenuse <= ball1.getRadius() + ball2.getRadius();
-		}
-		return false;
+	public void rotate(double rotateAngle, BouncingBall ball) {
+		rectToPolar(ball);
+		angle+=rotateAngle;
+		polarToRect(ball);
 	}
-
-
-	@Override
-	public void tick(double deltaT) {
-		isCollided = false;
-		//System.out.println("Start tick");
-
-		for (BouncingBall ball : ballList) {
-			ball.setX(ball.getX() + (ball.getVx() * deltaT));
-			ball.setY(ball.getY() + (ball.getVy() * deltaT));
-		}
-
-		for (BouncingBall ball : ballList) {
-			BouncingBall ball1 = ball;
-			for (BouncingBall balll : ballList) {
-				BouncingBall ball2 = balll;
-				if (!ball1.equals(ball2) && isColliding(ball1,ball2)) {
-					collision(ball1, ball2);
-					isCollided = true;
-				}
-			}
-
-		}
-
-		for (BouncingBall ball : ballList) {
-			if (ball.getX() < ball.getRadius() || ball.getX() > areaWidth - ball.getRadius()) {
-				System.out.println("in x wall");
-				isCollided = true;
-				ball.setVx(ball.getVx() * -1);
-			}
-			if (ball.getY() < ball.getRadius() || ball.getY() > areaHeight - ball.getRadius()) {
-				System.out.println("in y wall");
-				isCollided = true;
-				ball.setVy(ball.getVy() * -1);
-			}
-		}
-		if (!isCollided) {
-			for (BouncingBall ball : ballList) {
-				ball.setVy(ball.getVy() + deltaT * gravity);
-			}
+	
+	public void rectToPolar(BouncingBall ball) {
+		dist = Math.sqrt(ball.getVx()*ball.getVx()+ball.getVy()*ball.getVy());
+		angle= Math.atan(ball.getVy()/ball.getVx());
+		if (ball.getVx()<0){
+			angle+=Math.PI;
 		}
 	}
 
+	public void polarToRect(BouncingBall ball) {
+		ball.setVx(dist*Math.cos(angle));
+		ball.setVy(dist*Math.sin(angle));
+	}
+
+	
 	//För alla bollar i listan, lägg dem i myballs med rätt x, y, r.
 	@Override
 	public List<Ellipse2D> getBalls() {
